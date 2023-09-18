a4b429268689ffbb43e474f76233aa45955115fd
Leon Kurvers <info@leonkurvers.com>
2023-09-18 15:56:03 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
-import { DrinkButton } from "./components/DrinkButtons";
+import { DrinkButtons } from "./components/DrinkButtons";
 import { DrinkChoice } from "./components/DrinkChoice";
-import { coffee, tea } from "./utils/data";
+import { tea, coffee } from "./utils/data";
 
 export const App = () => {
+  const [userDrink, setUserDrink] = useState(tea);
+
   const greeting = "Welcome to our cafe!";
-  const userDrink = tea;
+
   return (
-    <div className="App">
+    <div className="app">
       {userDrink ? (
         <DrinkChoice drink={userDrink} />
       ) : (
         <>
           <h1>{greeting}</h1>
-          <DrinkButton drinkOne={tea.name} drinkTwo={coffee.name} />
+          <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
         </>
       )}
     </div>
--- src/components/DrinkButtons.jsx
+++ src/components/DrinkButtons.jsx
@@ -4,7 +4,9 @@ import { Button } from "./ui/Button";
 export const DrinkButton = ({ drinkOne, drinkTwo }) => {
   return (
     <>
-      <header>Would you like tea or coffee?</header>
+      <h2>
+        Would you like {drinkOne} or {drinkTwo}?
+      </h2>
       <div className="button-group">
         <Button text={drinkOne}></Button>
         <Button text={drinkTwo}></Button>
